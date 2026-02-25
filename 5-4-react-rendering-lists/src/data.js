74db08b7ad65d884830623e7668d9c9fdfca08c1
Reham-ms <s20227010@kfupm.edu.sa>
2026-02-25 11:43:53 +0300

    completing dueBadge

--- 5-4-react-rendering-lists/src/data.js
+++ 5-4-react-rendering-lists/src/data.js
@@ -3,8 +3,8 @@ export const sampleCourses = [
     id: "SWE-353",
     title: "Web Engineering",
     tasks: [
-      { id: "t1", title: "Read Ch. 3",  dueDate: "2025-10-09", isDone: false },
-      { id: "t2", title: "Map quiz",    dueDate: "2025-10-03", isDone: true  },
+      { id: "t1", title: "Read Ch. 3", dueDate: "2025-10-09", isDone: false },
+      { id: "t2", title: "Map quiz", dueDate: "2025-10-03", isDone: true },
     ],
   },
   {
@@ -16,7 +16,7 @@ export const sampleCourses = [
     id: "ICS-500",
     title: "Research Methodology",
     tasks: [
-      { id: "t3", title: "Make Presentation",    dueDate: "2025-10-06", isDone: false },
+      { id: "t3", title: "Make Presentation", dueDate: "2025-10-06", isDone: false },
     ],
   },
 ];
